85415f13a0c2b98bfcecc0d3c4f46164a589cf12
aditya mangal <adityamangal0202@gmail.com>
2021-03-12 09:06:07 +0530

    GFG Problems

---
+++ Copy the end.py
@@ -0,0 +1,5 @@
+# You are given a string str of length >= 2. You need to extract the last 2 characters and print a new string that has the extracted characters 3 times concatenated.
+
+# Slicing would be used 
+str = 'cat'
+print(str[-2:]+str[-2:]+str[-2:])
